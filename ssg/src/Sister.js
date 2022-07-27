cec6f979dbebc524f39967944d9a04d80ea69b53
최진석 <79374648+choiapple@users.noreply.github.com>
2022-07-27 12:26:21 +0900

    Create Sister.js

---
+++ ssg/src/Sister.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Sister({card, setCard, setPerson}){
+    const 아빠카드쓰기 = () => {
+        setCard(card-100000)
+        setPerson("sister")
+    }
+    
+    return (
+        <>
+        <p>딸</p>
+        <p>아빠 카드 한도는 : {card}</p>
+        <button onClick={아빠카드쓰기}>아빠카드 긁기 앗싸!</button>
+        </>
+    );
+
+}
+
+export default Sister;
