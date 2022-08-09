5e4aa088f2dec61b69df88317d107961b6a6d81d
choiapple <cj5255@naver.com>
2022-08-09 12:27:55 +0900

    Update Login.jsx

--- 8월9일/src/page/Login.jsx
+++ 8월9일/src/page/Login.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useContext } from 'react';
+import AuthContext from '../context/Auth.context';
 
 function Login() {
+
+    const auth = useContext(AuthContext)
+    console.log(auth.auth)
+    const handelLogin = () => {
+        auth.onLogin(
+           localStorage.setItem("isLogIn", !auth.auth),
+           auth.auth = !auth.auth
+        )
+       
+        console.log(localStorage)
+        console.log(auth.auth)
+
+    }
     return (
         <>
-            <h1>Login</h1>
+            <h1>
+                {
+                    auth.auth?"LogOut":"LogIn"
+                }
+            </h1>
+            <button onClick={handelLogin}>{auth.auth?"LogOut":"LogIn"}</button>
         </>
     );
 }
